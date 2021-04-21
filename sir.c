4f1a66cfc7e2cc8c2e157a5ef5e59c16940e9e5f
AKHIL SINGH <79986837+akhilsingh2003@users.noreply.github.com>
2021-04-21 22:20:35 +0530

    Add files via upload

---
+++ sir.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+  int a=2;
+   if(a>1)
+   {
+       if (a==2)
+    
+       printf("hii");
+   } 
+}
---
+++ size.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+    printf("%lu",sizeof(int));
+    return 0;
+}
